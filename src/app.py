6f5e8f6d60a1b98538bc8e315b8736bacaba0852
svenngjeto <svenn.gjeto@unit.no>
2019-11-04 11:22:43 +0100

    Added dir for context

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import json
 
 def handler(event, context):
     print(json.dumps(event))
-    print(context)
+    print(dir(context))
     return {
         "statusCode": 200,
         "body": json.dumps(event)

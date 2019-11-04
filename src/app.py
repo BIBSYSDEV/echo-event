b263c4a6a466c02cd81e3709932848edd00eea6e
svenngjeto <svenn.gjeto@unit.no>
2019-11-04 11:11:08 +0100

    No json for context

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import json
 
 def handler(event, context):
     print(json.dumps(event))
-    print(json.dumps(context))
+    print(context)
     return {
         "statusCode": 200,
         "body": json.dumps(event)

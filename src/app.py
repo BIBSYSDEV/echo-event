7ce20973b9e2764a8e254a8c156ad1d688bc22f5
svenngjeto <svenn.gjeto@unit.no>
2020-02-19 15:07:48 +0100

    new branch

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import json
 
 
 def handler(event, context):
-    print("Hei fra oppgradert versjon 3")
+    print("Hei fra oppgradert versjon 4")
     print(json.dumps(event))
     # print(dir(context))
     return {

0d0edb6fafbfeba04eedbd659928593abf96af32
svenngjeto <svenn.gjeto@unit.no>
2020-05-15 21:10:57 +0200

    Added extra dump method

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import json
 def handler(event, context):
     print("Hei fra oppgradert versjon 4")
     print(json.dumps(event))
-    # print(dir(context))
+    print(dir(context))
     return {
         "statusCode": 200,
         "body": json.dumps(event)
